7aa1bae0a2d4df45bf9950e0558197a723b09050
Aurelio Lopez Malo Vazquez De Lara <a0l05iw@MX.wal-mart.com>
2021-11-05 20:00:01 -0600

    Initial ModelTrackerTraining (first methods).
    Created Model tracker class for training, first building methods and
    properties defined for tracking model arguments.

--- models/builders.py
+++ models/builders.py
@@ -1,6 +1,9 @@
 import os
 os.chdir(os.environ['MAINPATH'])
 
+import itertools
+import contextlib as cm
+
 from sklearn import compose
 from sklearn import ensemble
 from sklearn import pipeline
@@ -55,6 +58,37 @@ class ModelPipelineBuilder:
         self.__init__(prep_trans=self.prep_trans)
 
 
-    
-    
+class ModelTrainingTracker:
 
+    def __init__(self, pipe:pipeline.Pipeline=None):
+        self.pipe = pipe
+        
+    def set_pipe(self, pipe:pipeline.Pipeline):
+        self.pipe = pipe
+    
+    def process_pipe_prep(self):
+        '''
+        Extract relevant arguments for preprocssing step in model pipeline.
+        '''
+        prep_column_transformer = dict(self.pipe.steps)['preprocessing']
+        transformations = prep_column_transformer.transformers
+        trans_steps = [pipe.steps for _, pipe, _ in transformations]
+        trans_steps = itertools.chain.from_iterable(trans_steps)
+        prep_kwargs = {name:repr(proc) for name, proc in trans_steps}
+        self.prep_kwargs = prep_kwargs
+
+    @property
+    def pipe(self):
+        return self.pipe
+    
+    @pipe.setter
+    def pipe(self, other):
+        self.pipe = other
+        if self.pipe is None:
+            self.prep_kwargs = {}
+        else:
+            self.process_pipe_prep()
+
+    @pipe.deleter
+    def pipe(self):
+        self.prep_kwargs = {}
